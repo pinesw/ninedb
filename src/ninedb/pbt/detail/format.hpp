68a60fed9f853a768e8603df4b5bf4c3f9ed6086
Wouter van Heeswijk <woutervh@gmail.com>
2024-06-03 15:44:43 +0200

    organize code

--- src/ninedb/pbt/detail/format.hpp
+++ src/ninedb/pbt/detail/format.hpp
@@ -16,62 +16,6 @@ namespace ninedb::pbt::detail
     constexpr uint32_t VERSION_MAJOR = 0;
     constexpr uint32_t VERSION_MINOR = 1;
 
-    struct NodeLeafShallow
-    {
-        uint64_t num_children;
-        std::string stem;
-
-        std::unique_ptr<uint8_t[]> data;
-        uint8_t *strings_ptr;
-        std::unique_ptr<uint64_t[]> string_offsets;
-
-        std::string_view suffix(uint64_t index) const
-        {
-            return std::string_view((char *)strings_ptr + string_offsets[num_children + index - 1], string_offsets[num_children + index] - string_offsets[num_children + index - 1]);
-        }
-
-        std::string_view value(uint64_t index) const
-        {
-            if (index == 0)
-            {
-                return std::string_view((char *)strings_ptr, string_offsets[0]);
-            }
-            else
-            {
-                return std::string_view((char *)strings_ptr + string_offsets[index - 1], string_offsets[index] - string_offsets[index - 1]);
-            }
-        }
-    };
-
-    struct NodeInternalShallow
-    {
-        uint64_t num_children;
-        std::string stem;
-
-        std::unique_ptr<uint8_t[]> data;
-        uint8_t *strings_ptr;
-        std::unique_ptr<uint64_t[]> offsets;
-        std::unique_ptr<uint64_t[]> num_entries;
-        std::unique_ptr<uint64_t[]> string_offsets;
-
-        std::string_view suffix(uint64_t index) const
-        {
-            return std::string_view((char *)strings_ptr + string_offsets[num_children + index - 1], string_offsets[num_children + index] - string_offsets[num_children + index - 1]);
-        }
-
-        std::string_view reduced_value(uint64_t index) const
-        {
-            if (index == 0)
-            {
-                return std::string_view((char *)strings_ptr, string_offsets[0]);
-            }
-            else
-            {
-                return std::string_view((char *)strings_ptr + string_offsets[index - 1], string_offsets[index] - string_offsets[index - 1]);
-            }
-        }
-    };
-
     struct Format
     {
         static const uint32_t MAGIC = 0x1EAF1111;
--- src/ninedb/pbt/detail/structures.hpp
+++ src/ninedb/pbt/detail/structures.hpp
@@ -47,4 +47,60 @@ namespace ninedb::pbt::detail
         std::vector<std::string> reduced_values;
         std::vector<std::string> suffixes;
     };
+
+    struct NodeLeafShallow
+    {
+        uint64_t num_children;
+        std::string stem;
+
+        std::unique_ptr<uint8_t[]> data;
+        uint8_t *strings_ptr;
+        std::unique_ptr<uint64_t[]> string_offsets;
+
+        std::string_view suffix(uint64_t index) const
+        {
+            return std::string_view((char *)strings_ptr + string_offsets[num_children + index - 1], string_offsets[num_children + index] - string_offsets[num_children + index - 1]);
+        }
+
+        std::string_view value(uint64_t index) const
+        {
+            if (index == 0)
+            {
+                return std::string_view((char *)strings_ptr, string_offsets[0]);
+            }
+            else
+            {
+                return std::string_view((char *)strings_ptr + string_offsets[index - 1], string_offsets[index] - string_offsets[index - 1]);
+            }
+        }
+    };
+
+    struct NodeInternalShallow
+    {
+        uint64_t num_children;
+        std::string stem;
+
+        std::unique_ptr<uint8_t[]> data;
+        uint8_t *strings_ptr;
+        std::unique_ptr<uint64_t[]> offsets;
+        std::unique_ptr<uint64_t[]> num_entries;
+        std::unique_ptr<uint64_t[]> string_offsets;
+
+        std::string_view suffix(uint64_t index) const
+        {
+            return std::string_view((char *)strings_ptr + string_offsets[num_children + index - 1], string_offsets[num_children + index] - string_offsets[num_children + index - 1]);
+        }
+
+        std::string_view reduced_value(uint64_t index) const
+        {
+            if (index == 0)
+            {
+                return std::string_view((char *)strings_ptr, string_offsets[0]);
+            }
+            else
+            {
+                return std::string_view((char *)strings_ptr + string_offsets[index - 1], string_offsets[index] - string_offsets[index - 1]);
+            }
+        }
+    };
 }
